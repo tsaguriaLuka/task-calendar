acf9b45eb564561114ff06432caf2dfe6afe706f
Luka <luka.tsaguria@icloud.com>
2024-06-01 05:26:29 +0300

    feat: day view drop and drag

--- components/base/VCalendarDayView.vue
+++ components/base/VCalendarDayView.vue
@@ -1,27 +1,46 @@
 <script setup lang="ts">
 import { storeToRefs } from "pinia";
 import { useEventsStore } from "~/stores/events";
+import { useMouseTracking } from "~/composables/mouse-move";
+import { useOnDragStart, useOnDrop } from "~/composables/events-drag-and-drop";
+import { useCalendar } from "~/composables/useCalendar";
 
 const { dayEvents } = storeToRefs(useEventsStore())
+
+const { mouse } = useMouseTracking()
+
+const {
+  year,
+  day,
+  month
+} = useCalendar()
+
+const date = new Date(year.value, month.value - 1, day.value, 0)
 </script>
 
 <template>
-  <div class="calendar__day">
+  <div
+    class="calendar__day"
+    @drop="useOnDrop($event, date, mouse.elementY)"
+    @dragover.prevent
+    @dragenter.prevent
+  >
     <VCalendarEvent
       v-for="event in dayEvents"
       :key="event.id"
       :event="event"
       :top="event.minutesStart"
       :height="event.duration"
+      @dragstart="useOnDragStart($event, event)"
+      draggable="true"
     />
   </div>
 </template>
 
 <style scoped lang="scss">
 .calendar__day {
-  :deep(.calendar-event) {
-    width: calc(100% - 80px);
-    left: 70px;
-  }
+  position: relative;
+  width: 100%;
+  height: 100%;
 }
 </style>
